621de5a0f890d1ea5fa2b6b77362a4a64ae319e5
Jahswill-web-dev <onuegbujahswill@gmail.com>
2026-01-07 11:52:49 +0100

    mades some changes to getsaascontext file

--- src/services/domain/getSaasContext.ts
+++ src/services/domain/getSaasContext.ts
@@ -1,20 +1,35 @@
 // import UserProfile, { IUserProfile } from "../../models/UserProfile";
 import { SaasAIProfile } from "../../models/SaasAIProfile";
-export async function getSaasContext(userId: string) {
-  const profile = await SaasAIProfile.findOne({ userId }).lean();
+import UserProfile, { IUserProfile } from "../../models/UserProfile";
 
+
+export async function getSaasContext(userId: string) {
+  const originalProfile = await UserProfile.findOne({ userId }).lean<IUserProfile>();
   
-  if (!profile) {
+  if (!originalProfile) {
     throw new Error("User SaaS profile not found");
   }
 
   return {
-    saasName: profile.saasName,
-    niche: profile.niche,
-    description: profile.productDescription,
-    target_audience: profile.targetAudience,
-    pain_points: profile.audiencePainPoints,
+    saasName: originalProfile.saasName,
+    productPromise: originalProfile.productPromise,
+    productDescription: originalProfile.productDescription,
+    target_audience: originalProfile.targetAudience,
+    painPoints: originalProfile.painPoints,
     // benefits: profile.benefits?.join(", "),
     // tone: profile.tone ?? "neutral",
   };
+}
+
+
+
+
+export async function getAIimpovedSaasContext(userId: string) {
+const profile = await SaasAIProfile.findOne({ userId }).lean();
+if(!profile){
+    throw new Error("User SaaS profile not found");
+}
+return {
+    content: profile.content,
+};
 }
